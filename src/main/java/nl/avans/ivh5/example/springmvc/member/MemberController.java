665b70890d400e0dd900c1da0f5d8f45a7cb4286
Robin Schellius <r.schellius@avans.nl>
2016-09-01 20:51:03 +0200

    Je kunt nu een Member opvragen. Layout verbeterd.

--- src/main/java/nl/avans/ivh5/example/springmvc/member/MemberController.java
+++ src/main/java/nl/avans/ivh5/example/springmvc/member/MemberController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 // @Secured("ROLE_USER")
@@ -32,7 +34,12 @@ class MemberController {
         return "members";
     }
 
-    @RequestMapping("/member")
+    /**
+     * Haal een lijst van Members en toon deze in een view.
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/member", method = RequestMethod.GET)
     public String listMembers(Model model) {
         // Zet de opgevraagde waarden in het model
         model.addAttribute("members", memberRepository.findAll());
@@ -41,4 +48,15 @@ class MemberController {
         // Open de juiste view template als resultaat.
         return "views/member/list";
     }
+
+    @RequestMapping(value = "/member/{id}", method = RequestMethod.GET)
+    public String listOneMember(Model model, @PathVariable int id) {
+        // Zet de opgevraagde waarden in het model
+        model.addAttribute("member", memberRepository.findMemberById(id));
+        // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
+        model.addAttribute("classActiveMember","active");
+        // Open de juiste view template als resultaat.
+        return "views/member/read";
+    }
+
 }
