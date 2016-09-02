30c8a6931b7f85821e1b88a89e0abfb1cb138cfe
Robin Schellius <r.schellius@avans.nl>
2016-09-02 20:06:29 +0200

    Updated, basis voor Spring applicatie.

--- library.sql
+++ library.sql
@@ -1,8 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `library`;
-
 -- --------------------------------------------------------
-
-USE `library`;
 
 --
 -- Database: `library`
@@ -10,15 +6,21 @@ USE `library`;
 
 -- --------------------------------------------------------
 
+DROP SCHEMA IF EXISTS `library` ;
+CREATE SCHEMA IF NOT EXISTS `library` DEFAULT CHARACTER SET latin1 ;
+USE `library` ;
+
 --
 -- Table structure for table `book`
 --
 
 CREATE TABLE `book` (
-  `ISBN` int(11) NOT NULL,
-  `Title` varchar(64) NOT NULL,
-  `Author` varchar(32) NOT NULL,
-  `Edition` int(11) NOT NULL
+  `ISBN` INT(11) UNSIGNED NOT NULL,
+  `Title` VARCHAR(64) NOT NULL,
+  `Author` VARCHAR(32) NOT NULL,
+  `Edition` INT(6) UNSIGNED NOT NULL,
+  `UpdatedDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ISBN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,19 +48,21 @@ INSERT INTO `book` (`ISBN`, `Title`, `Author`, `Edition`) VALUES
 --
 
 CREATE TABLE `copy` (
-  `CopyID` int(11) NOT NULL,
-  `LendingPeriod` int(11) NOT NULL,
-  `BookISBN` int(11) NOT NULL
+  `CopyID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+  `LendingPeriod` INT(11) NOT NULL,
+  `BookISBN` INT(11) UNSIGNED NOT NULL,
+  `UpdatedDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CopyID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+ALTER TABLE `copy` AUTO_INCREMENT = 1000;
 
---
--- Dumping data for table `copy`
---
+ALTER TABLE `copy`
+  ADD KEY `BookISBN` (`BookISBN`);
 
-INSERT INTO `copy` (`CopyID`, `LendingPeriod`, `BookISBN`) VALUES
-(10001, 5, 2222),
-(10002, 21, 1111);
+INSERT INTO `copy` (`LendingPeriod`, `BookISBN`) VALUES
+(5, 2222),
+(21, 1111);
 
 -- --------------------------------------------------------
 
@@ -67,17 +71,19 @@ INSERT INTO `copy` (`CopyID`, `LendingPeriod`, `BookISBN`) VALUES
 --
 
 CREATE TABLE `loan` (
-  `ReturnDate` date NOT NULL,
-  `MembershipNr` int(11) NOT NULL,
-  `CopyID` int(11) NOT NULL
+  `LoanDate` DATE NOT NULL,
+  `ReturnDate` DATE,
+  `MembershipNr` INT(11) UNSIGNED NOT NULL,
+  `CopyID` INT(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `loan`
---
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`ReturnDate`,`MembershipNr`,`CopyID`),
+  ADD KEY `MembershipNr` (`MembershipNr`),
+  ADD KEY `CopyID` (`CopyID`);
 
-INSERT INTO `loan` (`ReturnDate`, `MembershipNr`, `CopyID`) VALUES
-('2013-10-16', 1000, 10001);
+INSERT INTO `loan` (`LoanDate`, `MembershipNr`, `CopyID`) VALUES
+(NOW(), 1000, 1001);
 
 -- --------------------------------------------------------
 
@@ -86,25 +92,32 @@ INSERT INTO `loan` (`ReturnDate`, `MembershipNr`, `CopyID`) VALUES
 --
 
 CREATE TABLE `member` (
-  `MembershipNumber` int(11) NOT NULL,
-  `FirstName` varchar(32) NOT NULL,
-  `LastName` varchar(32) NOT NULL,
-  `Street` varchar(32) NOT NULL,
-  `HouseNumber` varchar(4) NOT NULL,
-  `City` varchar(32) NOT NULL,
-  `PhoneNumber` varchar(16) NOT NULL,
-  `EmailAddress` varchar(32) NOT NULL,
-  `Fine` double NOT NULL
+  `MembershipNumber` INT(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FirstName` VARCHAR(32) NOT NULL,
+  `LastName` VARCHAR(32) NOT NULL,
+  `Street` VARCHAR(32) NOT NULL,
+  `HouseNumber` VARCHAR(4) NOT NULL,
+  `City` VARCHAR(32) NOT NULL,
+  `PhoneNumber` VARCHAR(16) NOT NULL,
+  `EmailAddress` VARCHAR(32) NOT NULL,
+  `Fine` double NOT NULL,
+  `UpdatedDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`MembershipNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `member` AUTO_INCREMENT = 1000;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`MembershipNumber`, `FirstName`, `LastName`, `Street`, `HouseNumber`, `City`, `PhoneNumber`, `EmailAddress`, `Fine`) VALUES
-(1000, 'Pascal', 'van Gastel', 'Lovensdijkstraat', '61', 'Breda', '0765238754', 'ppth.vangastel@avans.nl', 0),
-(1001, 'Erco', 'Argante', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'e.argante@avans.nl', 0),
-(1002, 'Marice', 'Bastiaensen', 'Lovensdijkstraat', '63', 'Breda', '0765236789', 'mmcm.bastiaensen@avans.nl', 5);
+INSERT INTO `member` (`FirstName`, `LastName`, `Street`, `HouseNumber`, `City`, `PhoneNumber`, `EmailAddress`, `Fine`) VALUES
+('Pascal', 'van Gastel', 'Lovensdijkstraat', '61', 'Breda', '0765238754', 'ppth.vangastel@avans.nl', 0),
+('Erco', 'Argante', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'e.argante@avans.nl', 0),
+('Jan', 'Montizaan', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'j.montizaan@avans.nl', 0),
+('Frans', 'Spijkerman', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'f.spijkerman@avans.nl', 0),
+('Robin', 'Schellius', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'r.schellius@avans.nl', 0),
+('Marice', 'Bastiaensen', 'Lovensdijkstraat', '63', 'Breda', '0765236789', 'mmcm.bastiaensen@avans.nl', 5);
 
 -- --------------------------------------------------------
 
@@ -113,78 +126,51 @@ INSERT INTO `member` (`MembershipNumber`, `FirstName`, `LastName`, `Street`, `Ho
 --
 
 CREATE TABLE `reservation` (
+  `ReservationID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ReservationDate` date NOT NULL,
-  `MembershipNumber` int(11) NOT NULL,
-  `BookISBN` int(11) NOT NULL
+  `MembershipNumber` INT(11) UNSIGNED NOT NULL,
+  `BookISBN` INT(11) UNSIGNED NOT NULL,
+  `UpdatedDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY (`ReservationID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`ReservationDate`, `MembershipNumber`, `BookISBN`) VALUES
-('2013-09-29', 1001, 1111);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`ISBN`);
-
---
--- Indexes for table `copy`
---
-ALTER TABLE `copy`
-  ADD PRIMARY KEY (`CopyID`),
-  ADD KEY `BookISBN` (`BookISBN`);
-
---
--- Indexes for table `loan`
---
-ALTER TABLE `loan`
-  ADD PRIMARY KEY (`ReturnDate`,`MembershipNr`,`CopyID`),
-  ADD KEY `MembershipNr` (`MembershipNr`),
-  ADD KEY `CopyID` (`CopyID`);
-
---
--- Indexes for table `member`
---
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`MembershipNumber`);
-
---
--- Indexes for table `reservation`
---
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`ReservationDate`,`MembershipNumber`,`BookISBN`),
   ADD KEY `MembershipNumber` (`MembershipNumber`),
   ADD KEY `BookISBN` (`BookISBN`);
 
 --
--- Constraints for dumped tables
+-- Dumping data for table `reservation`
 --
+
+INSERT INTO `reservation` (`ReservationDate`, `MembershipNumber`, `BookISBN`) VALUES
+('2016-09-29', 1001, 1011);
 
 --
 -- Constraints for table `copy`
 --
 ALTER TABLE `copy`
-  ADD CONSTRAINT `copy_ibfk_1` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `copy_book` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `loan`
 --
 ALTER TABLE `loan`
-  ADD CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`MembershipNr`) REFERENCES `member` (`MembershipNumber`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`CopyID`) REFERENCES `copy` (`CopyID`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `loan_member` FOREIGN KEY (`MembershipNr`) REFERENCES `member` (`MembershipNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `loan_copy` FOREIGN KEY (`CopyID`) REFERENCES `copy` (`CopyID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`MembershipNumber`) REFERENCES `member` (`MembershipNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `reservation_member` FOREIGN KEY (`MembershipNumber`) REFERENCES `member` (`MembershipNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_book` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
 
+
+-- --------------------------------------------------------
+-- Gebruikersaccount maken voor Spring MVC applicatie
+--
+DROP USER 'spring'@'localhost';
+CREATE USER 'spring'@'localhost' IDENTIFIED BY 'test';
+
+GRANT ALL ON `library`.* TO 'spring'@'localhost';
