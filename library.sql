32859b8d1a697c17d6e0dfa3a3dfa6f183e1b46d
Robin Schellius <r.schellius@avans.nl>
2016-09-02 18:06:05 +0200

    Eerste versie van formulier.

--- library.sql
+++ library.sql
@@ -1,68 +1,190 @@
--- phpMyAdmin SQL Dump
--- version 4.5.0.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Gegenereerd op: 01 sep 2016 om 16:59
--- Serverversie: 5.6.20
--- PHP-versie: 5.6.14
+CREATE DATABASE IF NOT EXISTS `library`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- --------------------------------------------------------
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+USE `library`;
 
 --
--- Database: `spring`
+-- Database: `library`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `member`
+-- Table structure for table `book`
 --
 
-CREATE TABLE `member` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL
+CREATE TABLE `book` (
+  `ISBN` int(11) NOT NULL,
+  `Title` varchar(64) NOT NULL,
+  `Author` varchar(32) NOT NULL,
+  `Edition` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden geëxporteerd voor tabel `member`
+-- Dumping data for table `book`
 --
 
-INSERT INTO `member` (`id`, `name`, `email`) VALUES
-(1, 'Siva', 'siva@gmail.com'),
-(2, 'Prasad', 'prasad@gmail.com'),
-(3, 'Reddy', 'reddy@gmail.com'),
-(4, 'Robin Schellius', 'r.schellius@avans.nl'),
-(5, 'Jan Montizaan', 'j.montizaan@avans.nl'),
-(6, 'Frans Spijkerman', 'f.spijkerman@avans.nl');
+INSERT INTO `book` (`ISBN`, `Title`, `Author`, `Edition`) VALUES
+(1011, 'De avonden', 'Gerard van het Reve', 9),
+(1014, 'Het boek van violet en dood', 'Gerard Reve', 9),
+(1021, 'Ik heb nooit iets gelezen', 'Karel van het Reve', 2),
+(1111, 'Het leven is vurrukkulluk', 'Remco Campert', 1),
+(2222, 'De Ontdekking van de Hemel', 'Harry Mulisch', 5),
+(2223, 'De Aanslag', 'Harry Mulisch', 8),
+(3333, 'De Aanslag', 'Belastingdienst', 1),
+(4001, 'De geverfde vogel', 'Jerzy Kosinski', 2),
+(4003, 'Cockpit', 'Jerzy Kosinski', 1),
+(4005, 'Aanwezig', 'Jerzy Kosinski', 4),
+(8000, 'Ik, Jan Cremer', 'Jan Cremer', 22),
+(8888, 'Ik, Jan Klaassen', 'Herman Finkers', 22);
+
+-- --------------------------------------------------------
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Table structure for table `copy`
+--
+
+CREATE TABLE `copy` (
+  `CopyID` int(11) NOT NULL,
+  `LendingPeriod` int(11) NOT NULL,
+  `BookISBN` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Dumping data for table `copy`
+--
+
+INSERT INTO `copy` (`CopyID`, `LendingPeriod`, `BookISBN`) VALUES
+(10001, 5, 2222),
+(10002, 21, 1111);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+CREATE TABLE `loan` (
+  `ReturnDate` date NOT NULL,
+  `MembershipNr` int(11) NOT NULL,
+  `CopyID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `loan`
+--
+
+INSERT INTO `loan` (`ReturnDate`, `MembershipNr`, `CopyID`) VALUES
+('2013-10-16', 1000, 10001);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `MembershipNumber` int(11) NOT NULL,
+  `FirstName` varchar(32) NOT NULL,
+  `LastName` varchar(32) NOT NULL,
+  `Street` varchar(32) NOT NULL,
+  `HouseNumber` varchar(4) NOT NULL,
+  `City` varchar(32) NOT NULL,
+  `PhoneNumber` varchar(16) NOT NULL,
+  `EmailAddress` varchar(32) NOT NULL,
+  `Fine` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`MembershipNumber`, `FirstName`, `LastName`, `Street`, `HouseNumber`, `City`, `PhoneNumber`, `EmailAddress`, `Fine`) VALUES
+(1000, 'Pascal', 'van Gastel', 'Lovensdijkstraat', '61', 'Breda', '0765238754', 'ppth.vangastel@avans.nl', 0),
+(1001, 'Erco', 'Argante', 'Hogeschoollaan', '1', 'Breda', '0765231234', 'e.argante@avans.nl', 0),
+(1002, 'Marice', 'Bastiaensen', 'Lovensdijkstraat', '63', 'Breda', '0765236789', 'mmcm.bastiaensen@avans.nl', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `ReservationDate` date NOT NULL,
+  `MembershipNumber` int(11) NOT NULL,
+  `BookISBN` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`ReservationDate`, `MembershipNumber`, `BookISBN`) VALUES
+('2013-09-29', 1001, 1111);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `member`
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`ISBN`);
+
+--
+-- Indexes for table `copy`
+--
+ALTER TABLE `copy`
+  ADD PRIMARY KEY (`CopyID`),
+  ADD KEY `BookISBN` (`BookISBN`);
+
+--
+-- Indexes for table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`ReturnDate`,`MembershipNr`,`CopyID`),
+  ADD KEY `MembershipNr` (`MembershipNr`),
+  ADD KEY `CopyID` (`CopyID`);
+
+--
+-- Indexes for table `member`
 --
 ALTER TABLE `member`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`MembershipNumber`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`ReservationDate`,`MembershipNumber`,`BookISBN`),
+  ADD KEY `MembershipNumber` (`MembershipNumber`),
+  ADD KEY `BookISBN` (`BookISBN`);
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `member`
+-- Constraints for table `copy`
 --
-ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `copy`
+  ADD CONSTRAINT `copy_ibfk_1` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `loan`
+--
+ALTER TABLE `loan`
+  ADD CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`MembershipNr`) REFERENCES `member` (`MembershipNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`CopyID`) REFERENCES `copy` (`CopyID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`MembershipNumber`) REFERENCES `member` (`MembershipNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`BookISBN`) REFERENCES `book` (`ISBN`) ON UPDATE CASCADE;
+
--- src/main/java/nl/avans/ivh5/example/springmvc/member/MemberController.java
+++ src/main/java/nl/avans/ivh5/example/springmvc/member/MemberController.java
@@ -3,6 +3,8 @@ package nl.avans.ivh5.example.springmvc.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,12 +51,50 @@ class MemberController {
         return "views/member/list";
     }
 
+    /**
+     * Deze methode handelt een ingevuld formulier af. Als er fouten zijn opgetreden blijven we in dezelfde view.
+     * Als er geen fouten waren maken we een nieuwe member en gaan we direct naar de list view voor het overzicht.
+     * De nieuwe member moet dan in het overzicht staan.
+     *
+     * @param member De member uit het formulier. De velden van member komen uit de input velden van het formulier.
+     * @param bindingResult Het resultaat van de view.
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="/memberctrl", params={"save"})
+    public String saveMember(final Member member, final BindingResult bindingResult, final ModelMap model) {
+        if (bindingResult.hasErrors()) {
+            return "/member/create";
+        }
+        this.memberRepository.create(member);
+        model.clear();
+        return "redirect:/member";
+    }
+
+    /**
+     * Hiermee open je de create view om een nieuwe member aan te maken.
+     *
+     * @param member Dit object wordt aan de view meegegeven. Het object wordt gevuld met de waarden uit het formulier.
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="/member/create")
+    public String saveMember(final Member member, final ModelMap model) {
+        return "/views/member/create";
+    }
+
+    /**
+     * Haal het member met gegeven ID uit de database en toon deze in een view.
+     * @param model
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "/member/{id}", method = RequestMethod.GET)
     public String listOneMember(Model model, @PathVariable int id) {
         // Zet de opgevraagde waarden in het model
         model.addAttribute("member", memberRepository.findMemberById(id));
         // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
-        model.addAttribute("classActiveMember","active");
+        model.addAttribute("classActiveMember", "active");
         // Open de juiste view template als resultaat.
         return "views/member/read";
     }
