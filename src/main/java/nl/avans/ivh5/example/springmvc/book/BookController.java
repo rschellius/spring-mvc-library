ead03f3cf39842b9c742f810080b1bc2a5719555
Robin Schellius <r.schellius@avans.nl>
2016-09-07 23:30:00 +0200

    Foutafhandeling verbeterd

--- src/main/java/nl/avans/ivh5/example/springmvc/book/BookController.java
+++ src/main/java/nl/avans/ivh5/example/springmvc/book/BookController.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,6 +18,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class BookController {
 
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
+    private final String urlListAllBooks = "http://localhost:8090/api/book";
 
     /**
      * Maak een lijst met boeken waarin we kunnen zoeken.
@@ -33,7 +35,19 @@ public class BookController {
 
         // Aanroep naar de boekenserver - die moet wel draaien...
         RestTemplate restTemplate = new RestTemplate();
-        ArrayList<Book> books = restTemplate.getForObject("http://localhost:8090/api/book", ArrayList.class);
+        ArrayList<Book> books = null;
+        try {
+            books = restTemplate.getForObject(urlListAllBooks, ArrayList.class);
+        } catch (ResourceAccessException e) {
+            log.error(urlListAllBooks + " is not available - is the server running?");
+            // We gooien de exception door. Verderop in deze controller staat
+            // een handler hiervoor. Die handelt deze exception af.
+            throw e;
+        } catch (RestClientException e) {
+            log.error(urlListAllBooks + " is not available - is the server running?");
+            // Zie hierboven; wordt afgehandeld.
+            throw e;
+        }
 
         log.debug("findAllBooks", books);
 
@@ -45,4 +59,25 @@ public class BookController {
         return "views/book/list";
     }
 
+    /**
+     * Exception handler for this BookController. Deze handler vangt de genoemde
+     * exceptions - maar geen andere dus. Maakt specifieke afhandeling van fouten mogelijk.
+     *
+     * @param req
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler({ResourceAccessException.class, RestClientException.class})
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        log.error("Request " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("title", "Ooops :-(");
+        mav.addObject("lead", "Daar ging iets mis ...");
+        mav.addObject("message", "Het lijkt er op dat we de server op URL <b>" + urlListAllBooks + "</b> niet konden bereiken. Kan het zijn dat die nog niet gestart is?");
+        mav.addObject("exception", ex.getMessage());
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error/error");
+        return mav;
+    }
 }
