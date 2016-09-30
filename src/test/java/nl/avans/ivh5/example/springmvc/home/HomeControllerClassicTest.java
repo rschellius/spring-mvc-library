25f023037b9a30b4b41b55afa28f8299cb2b564c
Robin Schellius <r.schellius@avans.nl>
2016-09-30 23:04:49 +0200

    tussenversie

--- src/test/java/nl/avans/ivh5/example/springmvc/home/HomeControllerClassicTest.java
+++ src/test/java/nl/avans/ivh5/example/springmvc/home/HomeControllerClassicTest.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
